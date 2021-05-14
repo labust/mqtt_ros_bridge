e54f113561404d5de590a5db737376f66bc4dade
Ivan Lončar <ivan.loncar@fer.hr>
2021-05-14 05:51:31 +0200

    Converted ros message from Vector3Stamped to PointStamped as per recommendation. Added remap for local vehicle to lps (local positioning system). Moved pozyx creator software ip to launch file argument for easier changing on the vehicle.

--- src/pozyx_pos_bridge.py
+++ src/pozyx_pos_bridge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import PointStamped
 import bridge
 import rospy
 import signal
@@ -16,7 +16,7 @@ class pozyx_bridge(bridge.bridge):
         for tag in tags:
             if isinstance(tag, int):
                 ros_topic = '~' +hex(tag)
-                self.pos_publishers[tag] = rospy.Publisher(ros_topic, Vector3Stamped, queue_size=10)
+                self.pos_publishers[tag] = rospy.Publisher(ros_topic, PointStamped, queue_size=10)
             else:
                 print(tag + " is not a valid tag number. Check configuration.")
         bridge.bridge.__init__(self,mqtt_topic, client_id,user_id,password, host, port, keepalive)
@@ -30,10 +30,10 @@ class pozyx_bridge(bridge.bridge):
             if(int(msg_dict["tagId"]) in self.tags):
                 is_success = msg_dict["success"]
                 if(is_success):
-                    pos_message = Vector3Stamped()
-                    pos_message.vector.x = float(msg_dict["data"]["coordinates"]["x"])/1000
-                    pos_message.vector.y = float(msg_dict["data"]["coordinates"]["y"])/1000
-                    pos_message.vector.z = float(msg_dict["data"]["coordinates"]["z"])/1000
+                    pos_message = PointStamped()
+                    pos_message.point.x = float(msg_dict["data"]["coordinates"]["x"])/1000
+                    pos_message.point.y = float(msg_dict["data"]["coordinates"]["y"])/1000
+                    pos_message.point.z = float(msg_dict["data"]["coordinates"]["z"])/1000
 
                     pozyx_stamp = float(msg_dict["timestamp"])
                     #print(int(pozyx_stamp))
